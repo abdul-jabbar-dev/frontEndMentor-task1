879b3e764591faa6841e2322e64d0b811530a593
abduljabbar15 <abduljabber1532002@gmail.com>
2022-03-16 16:02:20 +0600

    submit task

--- interactive-pricing/src/components/LandPage.js
+++ interactive-pricing/src/components/LandPage.js
@@ -1,56 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './LandPage.css'
 const LandPage = () => {
     const dom = (tag) => document.querySelector(tag)
-    const [show, setShow] = useState(false)
+
     const [purchasePack, setPurchasePack] = useState({
-        pageviews: '',
-        price: '',
+        pageviews: '10K',
+        price: '16.00',
         yearly: true
     })
-    useEffect(() => console.log(purchasePack), [purchasePack,show])
 
-
-    const setPrice = e => {
-        dom('#pageviews').innerHTML = dom('#priceRange').value
-        if (dom('#priceRange').value === 0) {
+    const setPrice = (e) => {
+        console.log(e.target.value);
+        dom('#pageviews').innerHTML = e.target.value
+        if (e.target.value < 20) {
             dom('#pageviews').innerHTML = '0K'
-            dom('#price').innerHTML = '$0.00'
+            dom('#price').innerHTML = '0.00'
 
-        } else if (dom('#priceRange').value <= 20) {
+        } else if (e.target.value <= 20) {
             dom('#pageviews').innerHTML = '10K'
-            dom('#price').innerHTML = '$8.00'
+            dom('#price').innerHTML = '8.00'
 
-        } else if (dom('#priceRange').value <= 40) {
+        } else if (e.target.value <= 40) {
             dom('#pageviews').innerHTML = '50K'
-            dom('#price').innerHTML = '$12.00'
+            dom('#price').innerHTML = '12.00'
 
-        } else if (dom('#priceRange').value <= 60) {
+        } else if (e.target.value <= 60) {
             dom('#pageviews').innerHTML = '100K'
-            dom('#price').innerHTML = '$16.00'
+            dom('#price').innerHTML = '16.00'
 
-        } else if (dom('#priceRange').value <= 80) {
+        } else if (e.target.value <= 80) {
             dom('#pageviews').innerHTML = '500K'
-            dom('#price').innerHTML = '$24.00'
+            dom('#price').innerHTML = '24.00'
         } else {
             dom('#pageviews').innerHTML = '1M'
-            dom('#price').innerHTML = '$36.00'
+            dom('#price').innerHTML = '36.00'
         }
         purchasePack.pageviews = dom('#pageviews').innerHTML
         purchasePack.price = dom('#price').innerHTML
     }
-
+    // night mode dom style 
     const modeChng = e => {
         if (e.target.checked) {
             dom('#body').classList.add('darkMode')
             dom('*').classList.add('darkMode')
             dom('.textToggle').style.color = 'hsl(227,35%,75%)'
+            dom('#prices').style.color = 'hsl(227,35%,75%)'
             dom('#price').style.color = 'hsl(227,35%,75%)'
+
             dom('#submitButtn').style.backgroundColor = '#A0E6DE '
             dom('#submitButtn').style.color = '#141D2B '
             dom('.titleRoundLogo').style.stroke = 'rgba(207,216,239,0.1)'
         } else {
             dom('.textToggle').style.color = '#293356'
+            dom('#prices').style.color = '#293356'
             dom('#price').style.color = '#293356'
             dom('#body').classList.remove('darkMode')
             dom('#submitButtn').style.backgroundColor = '#293356 '
@@ -59,6 +61,15 @@ const LandPage = () => {
             dom('.titleRoundLogo').style.stroke = "rgb(207,216,239)"
         }
 
+    }
+    // alert result 
+    const getResult = () => {
+        return alert(`
+            Your package
+
+            Price: $${purchasePack.price}
+            Discount: $${purchasePack.yearly ? ((+purchasePack.price / 100) * 25) + '.00' : '0.00'}
+            Total Price: $${purchasePack.price - (purchasePack.yearly ? (+purchasePack.price / 100) * 25 : '')}.00`)
     }
 
     return (
@@ -96,11 +107,15 @@ const LandPage = () => {
 
                     </span>
                     <span>
-                        <h1 style={{ color: "hsl(227, 35%, 25%)" }} id="price" className="d-inline fw-bolder">$16.00</h1>
+                        <h1 className="d-inline fw-bolder " id='prices' style={{ color: '#293356' }}>
+                            $
+                            <h1 style={{ color: '#293356' }} className="d-inline fw-bolder" id='price'
+                            >16.00</h1>
+                        </h1>
                         <h6 className="d-inline"><small>/month</small></h6>
                     </span>
                 </div>
-                <input onChange={e => setPrice(e)} type="range" name="" id="priceRange" value="60" min="0" max="100" step="20" /><br />
+                <input onChange={e => setPrice(e)} type="range" name="priceRange" id="priceRange" min="0" max="100" step="20" /><br />
 
                 <span>
                     <p className="d-inline">Monthly Billing</p> &nbsp;
@@ -119,7 +134,7 @@ const LandPage = () => {
                     </ul>
                     <div>
                         <button id="submitButtn"
-                            onClick={() => setShow(!show)}
+                            onClick={() => getResult()}
                             style={{
                                 color: 'hsl(226, 100%, 87%)',
                                 border: 'none',
@@ -128,22 +143,7 @@ const LandPage = () => {
                             }} className="py-2 px-4 rounded-pill">Start my trial</button>
                     </div>
                 </div>
-                {show ? <div className="d-flex justify-content-around " id="result">`
-                    <div className="mt-5">
-                        <div className="d-flex ">
-                            <h6 className="d-inline">Price: &nbsp;</h6>
-                            <h6 className="d-inline">${!purchasePack.price && '00'}</h6>
-                        </div>
-                        <div className="d-flex">
-                            <h6 className="d-inline">Discount: &nbsp;</h6>
-                            <h6 className="d-inline">${purchasePack ? (+purchasePack.price.slice(1, 3) / 100) * 25 : '00'}.00</h6>
-                        </div>
-                        <div className="d-flex">
-                            <h6 className="d-inline">Total Price: &nbsp;</h6>
-                            <h6 className="d-inline">${+purchasePack.price.slice(1, 3) - purchasePack ? (+purchasePack.price.slice(1, 3) / 100) * 25 : '00'}.00</h6>
-                        </div>
-                    </div>
-                    `</div> : ''}
+
             </div>
         </body>
 
